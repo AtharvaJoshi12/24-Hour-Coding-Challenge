e4d5034e4390b8f08214e55b5045c9767d8caf99
Atharva <atharvajoshi202@gmail.com>
2023-10-29 01:30:22 +0530

    Add 02_dataTypes.js

---
+++ 02_dataTypes.js
@@ -0,0 +1,32 @@
+// Shortcut --->  nn bb ss u
+
+// There are 7 Primitive Datatypes in JavaScript
+// 1. Null
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. String
+// 6. Symbol
+// 7. Undefined
+
+let a = null;
+let b = 12;
+let c = BigInt("2332");
+let d = true;
+let e = "Atharva";
+let f = Symbol("123");
+let g;
+
+console.log(a, b, c, d, e, f, g);
+
+// Objects in JS
+
+const item = {
+  Bottle: 4,
+  Ball: 13,
+  Bat: 12,
+};
+
+console.log(item);
+console.log(item.Ball);
+console.log(item["Ball"]);
